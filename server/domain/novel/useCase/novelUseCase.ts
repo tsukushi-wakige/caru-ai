96b35462389d9ae4e47e0bbf08973ec161f8a064
caru-ini <caru17122@gmail.com>
2024-07-05 19:42:08 +0900

    Revert "feat: add novelUseCase"
    This reverts commit 8f004afbbb82fd0551e8066bef633aa0cf029e80.

--- server/domain/novel/useCase/novelUseCase.ts
+++
@@ -1,8 +0,0 @@
-import { transaction } from 'service/prismaClient';
-
-export const novelUseCase = {
-  scrape: (aozoraUrl: string): Promise<string> =>
-    transaction('RepeatableRead', async (tx) => {
-      return aozoraUrl;
-    }),
-};
